ace699de294a8fdc3e7b5c0a53e3ac974921949d
nedgarblansky <48221322+nedgarblansky@users.noreply.github.com>
2021-09-28 17:50:17 +0200

    Add files via upload

---
+++ LiczbaParzystaNieparzysta.java
@@ -0,0 +1,18 @@
+public class LiczbaParzystaNieparzysta {
+    public static void main(String[] args) {
+
+        // Program sprawdza czy podana "liczba" jest parzysta czy nieparzysta
+        // po czym wyświetla komunikat.
+
+            int liczba = 3;
+
+            if (liczba % 2 == 0) {
+                System.out.println("Liczba parzysta");
+            } else {
+                System.out.println("Liczba nieparzysta");
+            }
+    }
+
+
+}
+
